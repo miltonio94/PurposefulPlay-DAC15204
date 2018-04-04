5b33c8553f9f76e58fde380b9e82e7d7080412da
Mr. M <miltonio94@gmail.com>
2018-04-04 15:35:03 +0100

    wip

--- src/menu/Menu.js
+++ src/menu/Menu.js
@@ -4,7 +4,7 @@ import MiltonMenu from 'MiltonMenu'
 class Menu extends Component {
     <View>
         <View className='milton'>
-
+            <MiltonMenu />
         </View>
         <View className='michael'>
 
