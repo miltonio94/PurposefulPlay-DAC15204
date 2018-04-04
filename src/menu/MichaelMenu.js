2c9972ab79f7796235a701264388fdcb2955a8ff
shadzig <32872014+shadzig@users.noreply.github.com>
2018-04-04 15:35:01 +0100

    new stuff

---
+++ src/menu/MichaelMenu.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class MiltonMenu extends Component{
+    constructor(props){
+        super(props)
+
+    }
+    render(){
+
+    }
+}
+
+export default MiltonMenu
