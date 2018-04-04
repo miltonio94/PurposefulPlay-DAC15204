85f5fbf9b894b879fd2f680188bdc71c1a5e3d72
Mr. M <miltonio94@gmail.com>
2018-04-04 15:29:42 +0100

    skeleton for menu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        
       </View>
     );
   }
@@ -21,3 +19,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App
---
+++ src/menu/Menu.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import MiltonMenu from 'MiltonMenu'
+
+class Menu extends Component {
+    <View>
+        <View className='milton'>
+
+        </View>
+        <View className='michael'>
+
+        </View>
+    </View>
+}
